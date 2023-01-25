390800dfafd11bf315badb858a818afb467608d6
garaoh70 <122757758+garaoh70@users.noreply.github.com>
2023-01-25 23:15:42 +0900

    製品番号

---
+++ DomainDrivenDesign/2_5/ModelNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace DomainDrivenDesign.List2_5;
+
+public record class ModelNumber
+{
+	// private readonly string _productCode;
+	// private readonly string _branch;
+	// private readonly string _lot;
+	private readonly Lazy<string> _modelNumber;
+
+	public ModelNumber(string productCode, string branch, string lot)
+	{
+		//_productCode = productCode;
+		//_branch = branch;
+		//_lot = lot;
+        _modelNumber = new(() => $"{productCode}-{branch}-{lot}");
+    }
+
+	public override string ToString() => _modelNumber.Value;
+}
+
