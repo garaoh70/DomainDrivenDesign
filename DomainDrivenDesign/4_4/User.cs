2d21efa720178c3aedcf2fdd22791a9339650952
garaoh70 <tommoco.20031209@gmail.com>
2023-02-05 19:09:16 +0900

    ドメインサービス

---
+++ DomainDrivenDesign/4_4/User.cs
@@ -0,0 +1,13 @@
+﻿namespace DomainDrivenDesign.List4_4;
+
+public record class User
+{
+    public UserName UserName { get; init; }
+    public UserId UserId { get; init; }
+
+    public User(UserName userName)
+    {
+        UserName = userName;
+        UserId = new(Guid.NewGuid().ToString());
+    }
+}
---
+++ DomainDrivenDesign/4_4/UserId.cs
@@ -0,0 +1,15 @@
+﻿namespace DomainDrivenDesign.List4_4;
+
+public record class UserId
+{
+    public string Value { get; init; }
+
+    public UserId(string id)
+    {
+        if (string.IsNullOrEmpty(id))
+            throw new Exception("ID不正");
+        Value = id;
+    }
+
+    public override string ToString() => Value;
+}
