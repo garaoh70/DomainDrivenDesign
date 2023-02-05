fbd0095b19d0b2da0071eccf8ef88ac2204e278f
garaoh70 <122757758+garaoh70@users.noreply.github.com>
2023-02-05 19:09:16 +0900

    ドメインサービス

---
+++ DomainDrivenDesign/4_4/UserName.cs
@@ -0,0 +1,15 @@
+﻿namespace DomainDrivenDesign.List4_4;
+
+public record class UserName
+{
+    public string Value { get; init; }
+
+    public UserName(string userName)
+    {
+        if (userName.Length < 3)
+            throw new Exception("ユーザー名不正");
+        Value = userName;
+    }
+
+    public override string ToString() => Value;
+}
