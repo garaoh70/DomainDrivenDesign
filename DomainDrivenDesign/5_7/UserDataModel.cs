b073054a78866c3889f639d0bb07421907240ac8
garaoh70 <122757758+garaoh70@users.noreply.github.com>
2023-02-19 22:53:08 +0900

    EntityFramework を使って見た

---
+++ DomainDrivenDesign/5_7/UserDataModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DomainDrivenDesign.List5_7;
+
+[Table("users")]
+public class UserDataModel
+{
+    [Key]
+    [Required]
+    public string UserName { get; set; } = "";
+
+    [Required]
+    public string UserId { get; set; } = "";
+}
---
+++ DomainDrivenDesignUnitTest/UnitTest5_7.cs
@@ -0,0 +1,49 @@
+﻿using DomainDrivenDesign.List5_7;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace DomainDrivenDesignUnitTest;
+
+public class UnitTest5_7
+{
+    private string? _path = null;
+
+    [SetUp]
+    public void Setup()
+    {
+        _path = Path.GetTempFileName();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        if (!string.IsNullOrEmpty(_path))
+        {
+            try
+            {
+                File.Delete(_path);
+                _path = null;
+            }
+            catch
+            {
+            }
+        }
+    }
+
+    [Test]
+    public void EntityFramework()
+    {
+        using var db = new DatabaseContext(_path!);
+
+        var rdc = db.Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+        Assert.That(rdc, Is.Not.Null);
+        rdc.CreateTables();
+
+        db.Add(new UserDataModel() { UserName = "Garaoh70", UserId = $"{Guid.NewGuid()}" });
+        db.SaveChanges();
+
+        var s = db.Users.FirstOrDefault();
+        Assert.That(s, Is.Not.Null);
+        Assert.That(s.UserName, Is.EqualTo("Garaoh70"));
+    }
+}
